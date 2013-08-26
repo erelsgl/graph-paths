c7024f0b1629bb7e393b0094e2cc704e1fc16f16
erelsgl <erelsgl@gmail.com>
2013-08-26 20:00:09 +0300

    add example file

---
+++ example.js
@@ -0,0 +1,18 @@
+var best_paths = require('./graph-paths').best_paths;
+
+console.log("graph-paths.js demo start");
+
+var costs = [
+    [0,1,7,20],
+    [Infinity,0,4,19],
+    [Infinity,Infinity,0,7],
+    [Infinity,Infinity,Infinity,0],
+];
+
+console.log("costs: ");
+console.dir(costs);
+
+console.log("best paths from node #0:");
+console.dir(best_paths(costs, 0)); 
+
+console.log("graph-paths.js demo end");
--- graph-paths.js
+++ graph-paths.js
@@ -1,4 +1,64 @@
-var util = require("util");
+/**
+ * Find the shortest / cheapest paths in a dense graph.
+ * 
+ * @author Erel Segal-Halevi
+ * @since 2013-08
+ * @license GPL
+ */
+
+/**
+ * @param costs a square matrix, n-by-n, where element (i,j) is the direct cost from node i to node j.
+ * All costs are assumed to be non-negative.
+ * @param start one of the nodes (0...n-1).
+ * @return an array, where element j is: 
+ * 	{ cost: best_cost_from_i, path: best_path_from_i }
+ * Uses a simple dynamic programming algorithm. 
+ */
+var best_paths = function(costs, start) {
+	var numnodes = costs.length;
+
+	// initialization:
+	var numsteps = 1;
+	var costs_from_start_using_numsteps = [];
+	for (var targetnode=0; targetnode<numnodes; ++targetnode) {
+		costs_from_start_using_numsteps[targetnode] = {
+			cost: costs[start][targetnode],
+			path: targetnode==start? [start]: [start, targetnode]
+		}
+	}
+	
+	for (++numsteps; numsteps<numnodes; ++numsteps) {
+		var costs_from_start_using_numsteps_plus_1 = [];
+		for (var targetnode=0; targetnode<numnodes; ++targetnode) {
+			var cost_from_start_through_middle_using_numsteps_plus_1 = [];
+			for (var middlenode=0; middlenode<numnodes; ++middlenode) {
+				cost_from_start_through_middle_using_numsteps_plus_1[middlenode] = 
+					costs_from_start_using_numsteps[middlenode].cost +
+					costs[middlenode][targetnode];
+			}
+			var bestMiddlenode = argmin(cost_from_start_through_middle_using_numsteps_plus_1);
+			var bestcost =   cost_from_start_through_middle_using_numsteps_plus_1[bestMiddlenode];
+			if (bestcost != costs_from_start_using_numsteps[targetnode].cost) {
+				costs_from_start_using_numsteps_plus_1[targetnode] = {
+					cost: cost_from_start_through_middle_using_numsteps_plus_1[bestMiddlenode],
+					path: costs_from_start_using_numsteps[bestMiddlenode].path.concat([targetnode])
+				}
+			} else {
+				costs_from_start_using_numsteps_plus_1[targetnode] = costs_from_start_using_numsteps[targetnode]
+			}
+		}
+		costs_from_start_using_numsteps = costs_from_start_using_numsteps_plus_1;
+	}
+	
+	return costs_from_start_using_numsteps_plus_1;
+}
+
+module.exports = {
+	best_paths: best_paths
+}
+
+
+// UTILS:
 
 var argmax = function(array, iterator) {
 	var max = -Infinity;
@@ -24,75 +84,4 @@ var argmin = function(array, iterator) {
 		}
 	}
 	return arg;
-}
-
-/**
- * @param distances a square matrix, n-by-n, where element (i,j) is the direct distance from node i to node j.
- * All distances are assumed to be non-negative.
- * @param start one of the nodes (0...n-1).
- * @return an array, where element j is: 
- * 	{ distance: best_distance_from_i, path: best_path_from_i }
- * Uses a simple dynamic programming algorithm. 
- */
-var best_paths = function(distances, start) {
-	var numnodes = distances.length;
-
-	// initialization:
-	var numsteps = 1;
-	var distances_from_start_using_numsteps = [];
-	for (var targetnode=0; targetnode<numnodes; ++targetnode) {
-		distances_from_start_using_numsteps[targetnode] = {
-			distance: distances[start][targetnode],
-			path: targetnode==start? [start]: [start, targetnode]
-		}
-	}
-	console.dir(distances_from_start_using_numsteps);
-	
-	for (++numsteps; numsteps<numnodes; ++numsteps) {
-		var distances_from_start_using_numsteps_plus_1 = [];
-		for (var targetnode=0; targetnode<numnodes; ++targetnode) {
-			var distance_from_start_through_middle_using_numsteps_plus_1 = [];
-			for (var middlenode=0; middlenode<numnodes; ++middlenode) {
-				distance_from_start_through_middle_using_numsteps_plus_1[middlenode] = 
-					distances_from_start_using_numsteps[middlenode].distance +
-					distances[middlenode][targetnode];
-			}
-			var bestMiddlenode = argmin(distance_from_start_through_middle_using_numsteps_plus_1);
-			var bestDistance =   distance_from_start_through_middle_using_numsteps_plus_1[bestMiddlenode];
-			if (bestDistance != distances_from_start_using_numsteps[targetnode].distance) {
-				distances_from_start_using_numsteps_plus_1[targetnode] = {
-					distance: distance_from_start_through_middle_using_numsteps_plus_1[bestMiddlenode],
-					path: distances_from_start_using_numsteps[bestMiddlenode].path.concat([targetnode])
-				}
-			} else {
-				distances_from_start_using_numsteps_plus_1[targetnode] = distances_from_start_using_numsteps[targetnode]
-			}
-		}
-		distances_from_start_using_numsteps = distances_from_start_using_numsteps_plus_1;
-		console.dir(distances_from_start_using_numsteps);
-	}
-	
-	return distances_from_start_using_numsteps_plus_1;
-}
-
-module.exports = {
-	best_paths: best_paths
-}
-
-
-if (process.argv[1] === __filename) {
-	console.log("graph-paths.js demo start");
-
-	var distances = [
-        [0,1,7,20],
-	    [Infinity,0,4,19],
-	    [Infinity,Infinity,0,7],
-	    [Infinity,Infinity,Infinity,0],
-	];
-	
-	console.dir(distances);
-	
-	console.dir(best_paths(distances, 0)); 
-	
-	console.log("graph-paths.js demo end");
 }
