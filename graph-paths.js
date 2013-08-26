f4843833155f25f34e77bee229d7fa0b85980e07
erelsgl <erelsgl@gmail.com>
2013-08-26 19:01:14 +0300

    remove dependency in sylvester

--- graph-paths.js
+++ graph-paths.js
@@ -1,5 +1,4 @@
 var util = require("util");
-var sylvester = require('sylvester');
 
 var argmax = function(array, iterator) {
 	var max = -Infinity;
@@ -30,22 +29,20 @@ var argmin = function(array, iterator) {
 /**
  * @param distances a square matrix, n-by-n, where element (i,j) is the direct distance from node i to node j.
  * All distances are assumed to be non-negative.
- * @param start one of the nodes (1...n).
+ * @param start one of the nodes (0...n-1).
  * @return an array, where element j is: 
- * 	{ best_distance_from_i, best_path_from_i }
+ * 	{ distance: best_distance_from_i, path: best_path_from_i }
  * Uses a simple dynamic programming algorithm. 
  */
 var best_paths = function(distances, start) {
-	var numnodes = distances.cols();
-	if (numnodes != distances.rows())
-		throw new Error("distances matrix must be square");
+	var numnodes = distances.length;
 
 	// initialization:
 	var numsteps = 1;
 	var distances_from_start_using_numsteps = [];
-	for (var targetnode=1; targetnode<=numnodes; ++targetnode) {
-		distances_from_start_using_numsteps[targetnode-1] = {
-			distance: distances.e(start, targetnode),
+	for (var targetnode=0; targetnode<numnodes; ++targetnode) {
+		distances_from_start_using_numsteps[targetnode] = {
+			distance: distances[start][targetnode],
 			path: targetnode==start? [start]: [start, targetnode]
 		}
 	}
@@ -53,22 +50,22 @@ var best_paths = function(distances, start) {
 	
 	for (++numsteps; numsteps<numnodes; ++numsteps) {
 		var distances_from_start_using_numsteps_plus_1 = [];
-		for (var targetnode=1; targetnode<=numnodes; ++targetnode) {
+		for (var targetnode=0; targetnode<numnodes; ++targetnode) {
 			var distance_from_start_through_middle_using_numsteps_plus_1 = [];
-			for (var middlenode=1; middlenode<=numnodes; ++middlenode) {
-				distance_from_start_through_middle_using_numsteps_plus_1[middlenode-1] = 
-					distances_from_start_using_numsteps[middlenode-1].distance +
-					distances.e(middlenode, targetnode);
+			for (var middlenode=0; middlenode<numnodes; ++middlenode) {
+				distance_from_start_through_middle_using_numsteps_plus_1[middlenode] = 
+					distances_from_start_using_numsteps[middlenode].distance +
+					distances[middlenode][targetnode];
 			}
-			var bestMiddlenode = argmin(distance_from_start_through_middle_using_numsteps_plus_1)+1;
-			var bestDistance =   distance_from_start_through_middle_using_numsteps_plus_1[bestMiddlenode-1];
-			if (bestDistance != distances_from_start_using_numsteps[targetnode-1].distance) {
-				distances_from_start_using_numsteps_plus_1[targetnode-1] = {
-					distance: distance_from_start_through_middle_using_numsteps_plus_1[bestMiddlenode-1],
-					path: distances_from_start_using_numsteps[bestMiddlenode-1].path.concat([targetnode])
+			var bestMiddlenode = argmin(distance_from_start_through_middle_using_numsteps_plus_1);
+			var bestDistance =   distance_from_start_through_middle_using_numsteps_plus_1[bestMiddlenode];
+			if (bestDistance != distances_from_start_using_numsteps[targetnode].distance) {
+				distances_from_start_using_numsteps_plus_1[targetnode] = {
+					distance: distance_from_start_through_middle_using_numsteps_plus_1[bestMiddlenode],
+					path: distances_from_start_using_numsteps[bestMiddlenode].path.concat([targetnode])
 				}
 			} else {
-				distances_from_start_using_numsteps_plus_1[targetnode-1] = distances_from_start_using_numsteps[targetnode-1]
+				distances_from_start_using_numsteps_plus_1[targetnode] = distances_from_start_using_numsteps[targetnode]
 			}
 		}
 		distances_from_start_using_numsteps = distances_from_start_using_numsteps_plus_1;
@@ -86,16 +83,16 @@ module.exports = {
 if (process.argv[1] === __filename) {
 	console.log("graph-paths.js demo start");
 
-	var distances = $M([
+	var distances = [
         [0,1,7,20],
 	    [Infinity,0,4,19],
 	    [Infinity,Infinity,0,7],
 	    [Infinity,Infinity,Infinity,0],
-	]);
+	];
 	
 	console.dir(distances);
 	
-	console.dir(best_paths(distances, 1)); 
+	console.dir(best_paths(distances, 0)); 
 	
 	console.log("graph-paths.js demo end");
 }
