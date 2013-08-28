b23feff4114157eff4af07c4d4b4ad4bd924ab51
erelsgl <erelsgl@gmail.com>
2013-08-28 08:30:27 +0300

    add readme

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
-var cheapest_paths = require('./graph-paths').cheapest_paths;
-
 console.log("graph-paths.js demo start");
+
+var cheapest_paths = require('./graph-paths').cheapest_paths;
 
 /*
  * The network is defined by a matrix describing the cost of getting from node i to node j.
@@ -13,10 +13,11 @@ var costs = [
     [Infinity,Infinity,Infinity,0],
 ];
 
-console.log("costs matrix: ");
-console.dir(costs);
+var cheapest_paths_from_0 = cheapest_paths(costs, 0);
+console.log("cheapest paths from node #0 to all other nodes:");
+console.dir(cheapest_paths_from_0); 
 
-console.log("cheapest paths from node #0:");
-console.dir(cheapest_paths(costs, 0)); 
+console.log("cheapest path from node #0 to node #3:");
+console.dir(cheapest_paths_from_0[3]); 
 
 console.log("graph-paths.js demo end");
